8feefa34e3be5f3b3ce1ad9d9d99ce81980002a6
a-shamasneh <a.ashmasneh@gmail.com>
2017-01-07 10:05:27 +0200

    js-array-extention

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,16 @@
 // your code here
+var ExtendArray =function(){
+	var reparr={};
+	var element={};/// let say this object have keys and each key is represent index
+	reparr.first=function(){
+		///there is built in function (object.keys ) so this function return keys from object and push its to array 
+		var arr = Object.keys(element);
+		return arr[0];
+	}
+	reparr.last=function(){
+		
+		var arr = Object.keys(element);
+		return arr[arr.length-1];
+	}
+	return reparr;
+} 
