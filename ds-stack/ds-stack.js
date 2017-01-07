5292935f63a6c7679bb7f1874723135ec042bbb6
a-shamasneh <a.ashmasneh@gmail.com>
2017-01-07 09:44:14 +0200

    ds-stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,24 @@
 var Stack = function() {
 	//your code is here
+	var stack=Object.create(method);
+	stack.storage={};
+	stack.counter=0;
+	 return stack;
 };
+var method={};
+method.add=function(value){
+	 	this.storage[counter]=value;
+	 	this.counter++;
+	 }
+	 method.remove=function(){
+	 	if(Object.keys(this.storage).length>0){
+	 		var node=this.storage[this.counter];
+	 		delete this.storage[this.counter];
+	 		this.counter --;
+	 		return node;
+	 	}
+	 	return "list empty";
+	 }
+	 method._storage=function(){
+	 	return this.storage;
+	 }
